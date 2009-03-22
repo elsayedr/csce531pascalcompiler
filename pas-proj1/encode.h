354d465b4d5f9bd6ba7e6a405128890cd380a32d
Joshvanburen <Joshvanburen@b579c4b2-09e8-11de-b8f3-b9fb9eb71b04>
2009-03-22 02:58:31 +0000

    I've coded some stuff, but it does not work correctly right now.  Therefore, I've commented out the function call in tree.c thata calls the declareVariable function in encode.c.  I will debug tomorrow in an attempt to fix it.

---
+++ pas-proj1/encode.h
@@ -0,0 +1,12 @@
+#ifndef ENCODE_H
+#define ENCODE_H
+
+#include "types.h"
+#include "symtab.h"
+
+/*Function declarations*/
+void declareVariable(ST_ID, TYPE);
+int getAlignSize(TYPE);
+int getSkipSize(TYPE, int);
+
+#endif
