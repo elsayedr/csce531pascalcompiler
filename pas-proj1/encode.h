575339c790a2f363f29797c3ecc0edea6d27db12
Joshvanburen <Joshvanburen@b579c4b2-09e8-11de-b8f3-b9fb9eb71b04>
2009-04-08 05:56:01 +0000

    I changed a few things to make it closer to Fenner's naming conventions.

--- pas-proj1/encode.h
+++ pas-proj1/encode.h
@@ -5,10 +5,16 @@
 #include "symtab.h"
 #include "tree.h"
 
+/*External reference to variables in other files*/
+extern int base_offset_stack[BS_DEPTH];
+extern int bo_top;
+
 /*Function declarations*/
 void declareVariable(ST_ID, TYPE);
 int getAlignSize(TYPE);
 int getSkipSize(TYPE);
+void incrementStack();
+void decrementStack();
 
 /*Fenner's functions*/
 void enter_func_body(char * global_func_name, TYPE type, int loc_var_size);
@@ -16,5 +22,6 @@ void exit_func_body(char * global_func_name, TYPE type);
 void enter_main_body();
 void exit_main_body();
 void encode_expr(EXPR expr);
+int get_local_var_offset();
 
 #endif
